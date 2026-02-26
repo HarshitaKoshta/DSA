9577826f6bcfc5eb72eed9c4c0e87f650ac67f82
HarshitaKoshta <harshitakoshta2111@gmail.com>
2026-02-26 19:02:38 +0530

    prime check

---
+++ Sheets/PrimeCheck.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    bool isPrime = true;
+
+    if(n <= 1){
+        isPrime = false;
+    } else {
+        for(int i = 2; i*i<=n; i++){
+            if(n % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if(isPrime){
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+
+    return 0;
+}
