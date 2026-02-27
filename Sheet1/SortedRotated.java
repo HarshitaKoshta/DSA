2bfdcedbeb9c94d508d2fd99d12497959712ff03
HarshitaKoshta <harshitakoshta2111@gmail.com>
2026-02-27 19:57:49 +0530

    sorted rotated

---
+++ Sheet1/SortedRotated.java
@@ -0,0 +1,15 @@
+class SortedRotated {
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,1,2};
+        int count =0;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i]>arr[(i+1)%arr.length]){
+               count++;
+            }
+        } 
+        if(count <= 1)
+            System.out.println("Sorted");
+        else
+            System.out.println("Not Sorted");
+    }
+}
